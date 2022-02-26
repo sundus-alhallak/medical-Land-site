8c9800cd1f179e2bb7e64f23bf3d48c4ba0b9df1
Sundus MERCAN <77814446+sundus-alhallak@users.noreply.github.com>
2022-02-27 00:21:40 +0300

    Add files via upload

---
+++ Untitled-1.cs
@@ -0,0 +1,37 @@
+
+using system ;
+namespace degisken {
+
+    class program {
+
+        static void Main (string [] args ){
+         console.Writeline ("");
+         byte b =5;
+         sbyte   c =5;
+         short s = 5; 
+         ushort d = 5 ;
+         Int16 = 2;
+         Int32 = 5 ;
+         Int64 =7 ;
+         //reel sayilar 
+         folat f =8;
+         double g = 66;// 8 bayte
+         decimal de =3; 
+         char ch ='5'; //2 byte
+         string str ="zaas"// sinirsiz
+         bool b1= true;
+         boolb2 = false ;
+         DateTime dt= DateTime.now ;
+         console.Writeline(dt);
+
+
+
+        int int22 ="5";
+        string int33= " 6";
+        consule.writeline ("int22+int33")
+
+
+        }
+    }
+
+}
